d3333218cc37a0897e61625617cf2b94f5767a3a
JanCoe <jan@jancoe.io>
2025-05-27 00:55:16 +0100

    script to preview adoc

--- scripts/preview-adoc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# First parameter is the name of the file (without extension).
-
-# Convert to html
-asciidoctor $1.adoc
-# Render html
-xdg-open $1.html
--- symlinks/scripts-fedora.py
+++ symlinks/scripts-fedora.py
@@ -7,7 +7,7 @@ from . import utils
 def main() -> None:
     path_to_symlink = Path("/usr/local/bin")
     path_to_file = Path.home() / Path(".dotfiles/scripts")
-    names_symlink: list[str] = ["feh-background", "i3-status", "preview-adoc", "remove-kernels", "rofi-run-wayland", "rofi-run-x11",
+    names_symlink: list[str] = ["adocpre", "feh-background", "i3-status", "remove-kernels", "rofi-run-wayland", "rofi-run-x11",
                           "switch-display", "toggle-monitor", "update-fedora"]
     names_scripts = [name.join(".sh") for name in names_symlink]
 
